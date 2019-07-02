6ba3bc1a3bce2a2439ad4a4965e12e7254a67ca8
LuizHonorato <lhehonorato@gmail.com>
2019-07-01 21:50:27 -0300

    login basico.

---
+++ login-basico/resources/views/usuario.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+
+@section('content')
+@auth
+
+    <h4>Você está logado!</h4>
+    <p> {{ Auth::user()->name }} </p>
+    <p> {{ Auth::user()->email }} </p>
+    <p> {{ Auth::user()->id }} </p>
+
+@endauth
+
+@guest
+    <h4>Você não está logado!</h4>
+@endguest
+@endsection
